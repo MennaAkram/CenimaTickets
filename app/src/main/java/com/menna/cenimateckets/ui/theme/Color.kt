979627de8e4b76df2973add15b1d26798b9ab8fd
Mennaakram <mennaakram976@gmail.com>
2023-07-06 20:30:12 +0300

    add: colors

--- app/src/main/java/com/menna/cenimateckets/ui/theme/Color.kt
+++ app/src/main/java/com/menna/cenimateckets/ui/theme/Color.kt
@@ -2,15 +2,10 @@ package com.menna.cenimateckets.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
-val LightGrayTransparent = Color(0x4DCCCCCC)
+val LightGrayTransparent = Color(0x4D989185)
+val Gray = Color(0xFF989185)
 val Primary = Color(0xFFFF5524)
 val Border = Color(0xFFE5E5E5)
 val OnBackground_87 = Color(0xDE000000)
+val OnBackground_38 = Color(0x61000000)
+val Background = Color(0xFFffffff)
