2d9977f6633bf7d3252f51639bfa31cc6c987f46
Mennaakram <mennaakram976@gmail.com>
2023-07-06 23:54:34 +0300

    edit: ticket screen

--- app/src/main/java/com/menna/cenimateckets/composable/ticket/TicketState.kt
+++ app/src/main/java/com/menna/cenimateckets/composable/ticket/TicketState.kt
@@ -1,28 +1,33 @@
 package com.menna.cenimateckets.composable.ticket
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.menna.cenimateckets.R
+import com.menna.cenimateckets.ui.theme.Gray
 
 @Composable
 fun TicketState(
     text: String,
 ) {
-    Row() {
-        Image(painter = painterResource(R.drawable.circle),
-            contentDescription = "")
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
         Spacer(modifier = Modifier.width(8.dp))
-        Text(text = text)
+        Text(text = text, color = Gray)
     }
-
 }
 
 @Preview
--- app/src/main/java/com/menna/cenimateckets/screens/TicketScreen.kt
+++ app/src/main/java/com/menna/cenimateckets/screens/TicketScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,11 +17,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.menna.cenimateckets.R
-import com.menna.cenimateckets.composable.Time
 import com.menna.cenimateckets.composable.exitButton
 import com.menna.cenimateckets.composable.ticket.BottomSheet
-import com.menna.cenimateckets.composable.ticket.TicketState
-import com.menna.cenimateckets.ui.theme.LightGrayTransparent
 
 @Composable
 fun TicketScreen() {
@@ -35,7 +31,7 @@ fun TicketScreen() {
     ) {
         Row(modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 16.dp, vertical = 24.dp),
+            .padding(horizontal = 16.dp).padding(top = 24.dp),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Start
         ) {
@@ -44,12 +40,7 @@ fun TicketScreen() {
         Image(modifier = Modifier.fillMaxWidth(),
             painter = painterResource(R.drawable.subtract),
             contentDescription = "")
-Spacer(modifier = Modifier.height(300.dp))
-        Row() {
-            TicketState(text = "Available")
-            TicketState(text = "Taken")
-            TicketState(text = "Selected")
-        }
+Spacer(modifier = Modifier.height(310.dp))
         Spacer(modifier = Modifier.height(8.dp))
         BottomSheet()
     }
